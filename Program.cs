c272ef1a551fa3cd3c48c110e59c3060d67ee755
mchivari <frunzisoara@gmail.com>
2012-11-29 14:10:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameRunner.main(new String[2]);
+            System.Console.ReadKey();
+        }
+    }
+}
